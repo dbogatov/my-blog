fdb64c4e8cf78e8f3cbe02b9395b134c2fef6401
Dmytro <dbogatov@wpi.edu>
2016-10-10 03:25:09 -0400

    Fix.

--- _plugins/amp_generate.rb
+++ _plugins/amp_generate.rb
@@ -14,6 +14,7 @@ module Jekyll
       self.data['author']        = post.data['author']
       self.data['category']      = post.data['category']
       self.data['canonical_url'] = post.url
+	  self.data['image']         = post.data['image']
     end
   end
   # Generates a new AMP post for each existing post
