2d23007e9b502b6478f077358ec35e31e4926e51
Dmytro <dbogatov@wpi.edu>
2017-02-04 15:58:25 -0500

    Fix CI.

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ echo "Installing node modules... Requires NPM"
 npm install --loglevel=error > /dev/null
 
 echo "Installing Bower components... Requires Bower (installed by NPM)"
-$(npm bin)/bower install --allow-root > /dev/null
+$(npm bin)/bower install --allow-root > /dev/null 2>/dev/null
 
 echo "Removing node modules..."
 rm -rf node_modules > /dev/null
+
+echo "Installing gems... Requires Bundle"
+bundle install
 
 echo "Cleaning output directory... Requires Jekyll"
 jekyll clean
